c7bc1c955bfcbae633fc5c2685a5565dc9c9282a
TCK1997 <achinkhang97.sg@gmail.com>
2019-08-27 02:01:15 +0800

    Level 1 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Class for Duke.
  */
@@ -6,11 +8,41 @@ public class Duke {
      * Main method for duke.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String startUp =
+                  "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(startUp);
+        runDuke();
+    }
+
+    private static void runDuke() {
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            if (parseUserInput(userInput.nextLine())) {
+                break;
+            }
+        }
+    }
+
+    private static boolean parseUserInput(String userInput) {
+        String output = null;
+        String finalOutput;
+        boolean stop = false;
+        switch (userInput) {
+        case "bye":
+            output = "Bye. Hope to see you again soon!";
+            stop = true;
+            break;
+        default:
+            output = userInput;
+        }
+        finalOutput =
+                "    ____________________________________________________________\n"
+                + "     " + output + "\n"
+                + "    ____________________________________________________________\n";
+        System.out.println(finalOutput);
+        return stop;
     }
 }
--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,5 +1,5 @@
 public class DukeTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("test is okay.");
     }
 }
